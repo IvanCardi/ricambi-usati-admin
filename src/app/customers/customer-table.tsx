ce8cfc200f5b9b4d2eb4ddd609dad3efba9d4ecd
IvanCardi <ivancardillo1996@gmail.com>
2025-03-17 00:25:48 +0100

    fix: fixes empty tables placeholders

--- src/app/customers/customer-table.tsx
+++ src/app/customers/customer-table.tsx
@@ -133,7 +133,7 @@ export function CustomerTable<TData, TValue>({
                   colSpan={columns.length}
                   className="h-24 text-center"
                 >
-                  Nessuna macchina.
+                  Nessun Utente.
                 </TableCell>
               </TableRow>
             )}
